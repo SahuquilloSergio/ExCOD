da7a207f9d492baadec99c2ba26afb63c51ce3e1
ssahuquilloembade <ssahuquilloembade@victoria10.danielcastelao.org>
2017-11-17 08:52:36 +0100

    Commit B

--- src/excod1av/ExCOD1Av.java
+++ src/excod1av/ExCOD1Av.java
@@ -3,7 +3,7 @@ package excod1av;
 public class ExCOD1Av {
 
     public static void main(String[] args) {
-        
+        System.out.println("Este es un codigo para el commit B");
     }
     
 }
