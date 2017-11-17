235bd5970ffb78d28a486bec72b6464bb14eef38
ssahuquilloembade <ssahuquilloembade@victoria10.danielcastelao.org>
2017-11-17 09:02:37 +0100

    Commit D Rama EXP

--- src/excod1av/ExCOD1Av.java
+++ src/excod1av/ExCOD1Av.java
@@ -3,8 +3,8 @@ package excod1av;
 public class ExCOD1Av {
 
     public static void main(String[] args) {
-        System.out.println("Este es un codigo para el commit B");
-         System.out.println("Rama exp commit C");
+        
+        System.out.println("Rama exp commit C");
     }
     
 }
